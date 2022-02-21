90f8f02bf5e45577047b5ad3139eebb6d714a0dc
erlichdominik <mimikdesu@gmail.com>
2022-02-21 12:45:32 +0100

    bootstrap BlogComponent project

---
+++ BlogComponent/src/main/java/dev/erlichdominik/blogcomponent/BlogComponentApplication.java
@@ -0,0 +1,13 @@
+package dev.erlichdominik.blogcomponent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogComponentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogComponentApplication.class, args);
+    }
+
+}
---
+++ BlogComponent/src/test/java/dev/erlichdominik/blogcomponent/BlogComponentApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.erlichdominik.blogcomponent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogComponentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
